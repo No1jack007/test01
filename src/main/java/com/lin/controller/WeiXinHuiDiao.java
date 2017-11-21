78d46a1914f067dd458d278b5343c732d9f688d8
No1jack007 <2322575520@qq.com>
2017-11-21 11:27:56 +0800

    测试项目

---
+++ src/main/java/com/lin/controller/WeiXinHuiDiao.java
@@ -0,0 +1,12 @@
+package com.lin.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by zhang yufei on 2017/8/1.
+ */
+@Controller
+
+public class WeiXinHuiDiao {
+
+}
